9166ae81c36374ae6e3e91920495fab970458aec
Marius Pedersen <marius.pedersen95@hotmail.no>
2022-05-26 19:38:44 +0200

    Sass fix and text style

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,28 +2,33 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    app: {
-        head: {
-          title: 'etertainment-app',
-          meta: [
-            // <meta name="viewport" content="width=device-width, initial-scale=1">
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-          ],
-          script: [
-            // <script src="https://myawesome-lib.js"></script>
-            { src: 'https://awesome-lib.js' }
-          ],
-          link: [
-            // <link rel="stylesheet" href="https://myawesome-lib.css">
-            { rel: 'stylesheet', href: 'https://awesome-lib.css',
-              rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' 
-            }
-          ],
-          // please note that this is an area that is likely to change
-          style: [
-            // <style type="text/css">:root { color: red }</style>
-            { children: ':root { color: black }', type: 'text/css' }
-          ]
-        }
-      }
+  app: {
+    head: {
+      title: 'entertainment-app',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      script: [],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://awesome-lib.css',
+        },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap',
+        },
+      ],
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/scss/import";`,
+        },
+      },
+    },
+  },
 })

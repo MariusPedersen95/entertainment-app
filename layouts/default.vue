7953db8bed5c3dbc35d2679aff00649cee4d8cf1
Marius Pedersen <marius.pedersen95@hotmail.no>
2022-05-29 21:45:48 +0200

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Nav />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  height: 100%;
+}
+
+main {
+  padding: 0 0 0 6rem;
+}
+</style>
